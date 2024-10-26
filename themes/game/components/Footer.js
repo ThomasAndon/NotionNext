fef70a8d414be9606d0357031bcc9d60c4257669
ThomasAndon <513230863@qq.com>
2024-10-26 18:34:45 +0800

    impr: Changed UI

--- themes/game/components/Footer.js
+++ themes/game/components/Footer.js
@@ -19,13 +19,8 @@ export const Footer = props => {
         <p>{siteConfig('DESCRIPTION')}</p>
 
         <span className='dark:text-gray-200 no-underline ml-4'>
-          Powered by
-          <a
-            href='https://github.com/tangly1024/NotionNext'
-            className=' hover:underline'>
-            {' '}
-            NotionNext {siteConfig('VERSION')}{' '}
-          </a>
+          Powered by NotionNext
+
         </span>
       </div>
     </footer>
